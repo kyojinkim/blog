f1126d5b33d9af2ba571771b66f4946c0169ecc3
Kyo Jin Kim <kyojin.kim@gmail.com>
2023-01-10 16:08:45 +0900

    initial

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
+	}
+};
+
+export default config;
